a08e53389c9cc089aeb6900d66659b6fd7d2b8c0
baomanliu <139863668+baomanliu@users.noreply.github.com>
2023-09-18 11:02:56 +0800

    Add files via upload

---
+++ user.py
@@ -0,0 +1,23 @@
+USER_THEMES = {
+    "seer1": {
+        "type": "light",
+        "colors": {
+            "primary": "#78c2ad",
+            "secondary": "#f3969a",
+            "success": "#56cc9d",
+            "info": "#6cc3d5",
+            "warning": "#ffce67",
+            "danger": "#ff7851",
+            "light": "#F8F9FA",
+            "dark": "#6a380b",
+            "bg": "#feeac7",
+            "fg": "#5a5a5a",
+            "selectbg": "#f3969a",
+            "selectfg": "#ffffff",
+            "border": "#ced4da",
+            "inputfg": "#696969",
+            "inputbg": "#fff",
+            "active": "#e5e5e5"
+        }
+    }
+}
